4f5bb72de45a05dfb4682e62ff754169ab19fb28
Dejan43 <dejandragojlovic862@gmail.com>
2025-10-13 09:24:31 +0200

    Remove old scripts and mergeReturn.cpp

--- generate_ll_for_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-../../../../build/bin/clang -emit-llvm -S tests/test1.cpp -o tests/test1.ll
-../../../../build/bin/clang -emit-llvm -S tests/test2.cpp -o tests/test2.ll
-../../../../build/bin/clang -emit-llvm -S tests/test3.cpp -o tests/test3.ll
-
-../../../../build/bin/opt -load ../../../../build/lib/LLVMMergeReturnFunctionPass.so -enable-new-pm=0 -mergereturn tests/test1.ll -o tests/output1.ll -S
-../../../../build/bin/opt -load ../../../../build/lib/LLVMMergeReturnFunctionPass.so -enable-new-pm=0 -mergereturn tests/test2.ll -o tests/output2.ll -S
-../../../../build/bin/opt -load ../../../../build/lib/LLVMMergeReturnFunctionPass.so -enable-new-pm=0 -mergereturn tests/test3.ll -o tests/output3.ll -S
